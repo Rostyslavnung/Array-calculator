cf8d3d2e8732d64b1c75367b23275285e1e143c4
Rostyslavnung <153272975+Rostyslavnung@users.noreply.github.com>
2023-12-17 11:35:43 +0200

    Headers

---
+++ Header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Size.h"
+
+void fillA(int array[size]);
+void print_Array(int array[size]);
+void fillC(int A[size], int C[size]);
+void conditionalFunc(int A[size], int C[size], int isOdd = 0);
